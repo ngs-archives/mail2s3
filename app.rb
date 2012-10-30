8bf737cb9cdae5a11d0338190f46aba815151a70
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-10-31 04:07:23 +0900

    debug

--- app.rb
+++ app.rb
@@ -12,12 +12,20 @@ post '/incoming' do
   dir = "mail2s3/#{ now.strftime('%Y%m%d') }"
   filename = "#{ mail.message_id } #{ mail.subject }"
   filename = "No subject #{ now.strftime('%H%M') }" if filename.blank?
-  text = mail.text_part ? mail.text_part.body.to_s : nil
-  html = mail.html_part ? mail.html_part.body.to_s : nil
+  if mail.multipart?
+    text = mail.text_part ? mail.text_part.body.to_s : nil
+    html = mail.html_part ? mail.html_part.body.to_s : nil
+  elsif mail.content_type == 'text/plain'
+    text = mail.body
+  elsif mail.content_type == 'text/html'
+    html = mail.body
+  end
 
   AWS::S3::S3Object.store("#{dir}/#{filename}.html", html, settings.bucket_name) if html && !html.blank?
   AWS::S3::S3Object.store("#{dir}/#{filename}.txt",  text, settings.bucket_name) if text && !text.blank?
 
+  p mail.body
+  p mail.content_type
   p text
   p html
 
